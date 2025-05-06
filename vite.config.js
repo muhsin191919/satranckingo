6be0803e9a005149e8ba024d59c8e0741f1b038b
muhsin191919 <ankara558@gmail.com>
2025-05-06 03:41:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // proje kökü burası
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
-    },
+    emptyOutDir: true,
   },
+  publicDir: 'public', // public klasörünü belirt!
 })
